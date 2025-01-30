341f3327dabef6347f773e9436ffec3343df8c4a
A1hame <176008065+A1hame@users.noreply.github.com>
2025-01-30 09:27:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const titleText = "A 1 h a m | S o p h i e e";
+const titleText = "A 1 h a m ";
 let index = 0;
 
 function animateTitle() {
     if (index <= titleText.length) {
         document.title = titleText.substring(0, index);
         index++;
-        setTimeout(animateTitle, 200); // Speed of animation
+        setTimeout(animateTitle, 100); // Speed of animation
     }
 }
 
