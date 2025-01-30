702538b63fe893030a985e56feeed6ef791cf4c8
A1hame <176008065+A1hame@users.noreply.github.com>
2025-01-30 13:54:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const titleText = "A1ham";
-let index = 0;
-
-function animateTitle() {
-    if (index <= titleText.length) {
-        document.title = titleText.substring(0, index);
-        index++;
-        setTimeout(animateTitle, 100); // Speed of animation
-    }
-}
-
-window.onload = animateTitle;
+Using Script.Js From Eclipsed.Top/Aiham
